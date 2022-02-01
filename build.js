fe7daab740dc4fee8598f8e682b89f2a23748a7d
Steinar Logi <steinarlg@gmail.com>
2022-02-01 20:09:19 +0000

    prófa hinar skrárnar

--- build.js
+++ build.js
@@ -8,6 +8,10 @@ import { parseData } from './scripts/parse-data.js';
 const PATH = './data';
 const OUTPUTDIR = './dist';
 
+function makeName(name) {
+  return name.split('.').splice(0, name.split('.').length-1) + '.html';
+}
+
 async function main() {
   const files = await readdir(PATH);
   const filelistHtml = makeHtmlList(files);
@@ -18,8 +22,14 @@ async function main() {
     const stats = parseData(data.toString('utf-8'));
   }
 
-  const outputPath = join(OUTPUTDIR, "index.html"); 
+  const outputPath = join(OUTPUTDIR, "index.html");
   await writeFile(outputPath, indexTemplate(filelistHtml));
+
+  for(const file of files) {
+    const path = join(OUTPUTDIR, makeName(file));
+    console.log(path);
+    await writeFile(path, "Hello world!");
+  }
 }
 
 main().catch((err) => console.log(err));
